1d7501cf3cfad499c7f14ba2106ccf2adc97c5c5
Simar <ssimarsingh7@gmail.com>
2025-05-04 23:54:14 +0530

    Updated database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,7 +3,6 @@ mongoose.set('strictQuery', false);
 const MONGO_URI = process.env.MONGO_URI;
 
 const connectDatabase = () => {
-    console.log("Mongo URI:", MONGO_URI);
     mongoose.connect(MONGO_URI)
         .then(() => {
             console.log("Mongoose Connected");
